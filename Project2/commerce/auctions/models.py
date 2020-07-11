9a6e69bf6d114b173abca0e679803078829df76a
Ricardo Peixoto <ricardo.peixoto@usp.br>
2020-07-10 22:44:06 -0300

    Add models

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class Bid(models.Model):
+    value = models.DecimalField(max_digits=11, decimal_places=2)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid")
+    def __str__(self):
+      return f"${self.value} from {self.owner}"
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=560)
+    active = models.BooleanField(default = True)
+    image =models.URLField(null = True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auction")
+    interested = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist", null=True)
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="auction")
+    def __str__(self):
+      return f"{self.title} \n{self.description} \nBid: {self.bid}"
+
+class Comment(models.Model):
+    message = models.CharField(max_length=64)
+    date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comment")
+    def __str__(self):
+      return f"Comment of {self.owner} on {self.date} \n{self.message}"
--- Project2/commerce/auctions/views.py
+++ Project2/commerce/auctions/views.py
@@ -61,3 +61,9 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def createlist(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "auctions/register.html")
