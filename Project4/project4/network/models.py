fcee5984d7207bc1719755c3f39759a3d0808c98
Ricardo Peixoto <ricardo.peixoto@usp.br>
2020-07-22 16:59:22 -0300

    Add models

--- Project4/project4/network/models.py
+++ Project4/project4/network/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField("self", blank=True, related_name="following")
+
+    def __str__(self):
+      return f"{self.username}"
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=240)
+    likes = models.ManyToManyField(User, blank=True, related_name="liked")
+
+    def __str__(self):
+      return f"Post by {self.owner} at " + self.date.strftime("%b %d %Y, %I:%M %p")
